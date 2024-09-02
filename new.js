bd45d66210a8b77a263494051b70d9d999eb74c8
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-09-02 11:28:49 +0600

    ternary

---
+++ new.js
@@ -0,0 +1,8 @@
+var biriyani = 250;
+
+if(biriyani<300){
+  console.log("Give me 1 packet biriyani")
+}
+else{
+  console.log("I don't need biriyani")
+}
---
+++ ternary.js
@@ -0,0 +1,18 @@
+var age = 20;
+age >= 18 ? console.log("Vote Dio"): console.log("Ghumao");
+
+let price = 500;
+const isLeader = true;
+
+if(isLeader == true){
+  price = 0;
+}
+else{
+  price = price + 100
+}
+console.log(price)
+
+
+price = isLeader == true ? 0:price+100;
+console.log(price)
+
